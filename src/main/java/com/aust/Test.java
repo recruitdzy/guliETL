9997e3a41db8eb1175b8c5dad391e0b60459c1e1
dzy <178.com>
2022-05-12 11:54:18 +0800

    第一次提交

--- src/main/java/com/aust/Test.java
+++ src/main/java/com/aust/Test.java
@@ -3,8 +3,7 @@ package com.aust;
 public class Test {
     public static void main(String[] args) {
 
-        String str = ETLUtil.etlStr("fQShwYqGqsw	lonelygirl15	736	People & Blogs	133	151763	3.01	666	765	fQShwYqGqsw	LfAaY1p_2Is	5LELNIVyMqo	vW6ZpqXjCE4	vPUAf43vc-Q	ZllfQZCc2_M	it2d7LaU_TA	KGRx8TgZEeU	aQWdqI1vd6o	kzwa8NBlUeo	X3ctuFCCF5k	Ble9N2kDiGc	R24FONE2CDs	IAY5q60CmYY	mUd0hcEnHiU	6OUcp6UJ2bA	dv0Y_uoHrLc	8YoxhsUMlgA	h59nXANN-oo	113yn3sv0eo");
-        System.out.println(str);
+        System.out.println("第一次提交");
 
 
     }
